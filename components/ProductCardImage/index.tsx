9943b6a37c7982d1517816b455c7ed28c4845015
Andrey Grebeshov <andrey.grebeshov@gmail.com>
2023-08-11 10:37:23 +0300

    feat: productCard image

--- components/ProductCardImage/index.tsx
+++ components/ProductCardImage/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import classNames from 'classnames'
 
 import Image from '../ui/Image'
 
@@ -6,100 +7,78 @@ import { ProductCardImageProps } from './types'
 
 import styles from './styles.module.scss'
 
+const maxImages = 10
+
 const ProductCardImage = ({ imageList }: ProductCardImageProps) => {
 
-	const lastCloneRef = useRef<HTMLDivElement | null>(null)
-	const firstCloneRef = useRef<HTMLDivElement | null>(null)
+	const imagesRef = useRef<Array<HTMLDivElement | null>>([])
 	const imageListRef = useRef<HTMLDivElement | null>(null)
 
-	const initialPictureList = useMemo(() => {
-		if (imageList.length > 1) {
-			return [
-				{
-					id: 'lastClone',
-					ref: lastCloneRef,
-					...imageList[imageList.length - 1]
-				},
-				...imageList.map((image, index) => ({
-					id: String(index),
-					ref: undefined,
-					...image
-				})),
-				{
-					id: 'firstClone',
-					ref: firstCloneRef,
-					...imageList[0]
-				}
-			]
-		}
+	const [activeImageId, setActiveImageId] = useState(0)
 
-		return imageList.map((image, index) => ({ id: String(index), ref: undefined, ...image }))
-	}, [imageList])
-
-	const [pictureList, setpictureList] = useState(initialPictureList)
-	const [mount, setMount] = useState(false)
-
-	const callbackObserver: IntersectionObserverCallback = (entries) => {
-
-		entries.map(entry => {
-
-			const widthItem = lastCloneRef.current?.offsetWidth
-
-			if (entry.target.id === 'lastClone') {
-				imageListRef.current?.classList.add('overflow--clear')
-				widthItem && imageListRef.current?.scrollTo(widthItem * imageList.length + 1, 0)
-				imageListRef.current?.classList.remove('overflow--clear')
-			}
-
-			if (entry.target.id === 'firstClone') {
-				imageListRef.current?.classList.add('overflow--clear')
-				widthItem && imageListRef.current?.scrollTo(widthItem, 0)
-				imageListRef.current?.classList.remove('overflow--clear')
+	const callbackObserver: IntersectionObserverCallback = useCallback((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				setActiveImageId(Number(entry.target.getAttribute('id')))
 			}
 		})
-	}
+	}, [imageList])
 
-	if (mount) {
+	const initialObserve = useCallback(() => {
 
 		const observer = new IntersectionObserver(callbackObserver, {
 			root: imageListRef.current,
 			threshold: 1
 		})
 
-		lastCloneRef.current && observer.observe(lastCloneRef.current)
-		firstCloneRef.current && observer.observe(firstCloneRef.current)
-	}
+		imagesRef.current.forEach((imageRef) => imageRef && observer.observe(imageRef))
+	}, [imageList])
 
 	useEffect(() => {
-		setMount(true)
-	}, [])
+		imageList.length > 1 && initialObserve()
+	}, [imageList])
 
 	return (
-		<div className={styles['layout']}>
-			<div
-				className={styles['imageList']}
-				ref={imageListRef}
-			>
-				{
-					pictureList.map(image =>
-						<div
-							className={styles['image']}
-							id={image.id}
-							ref={image.ref}
-							key={image.id}
-						>
-							<Image
-								src={image.src}
-								alt={image.alt}
-								fill
-								style={{ objectFit: 'contain' }}
-							/>
-						</div>
-					)
-				}
-
+		<div className={classNames(imageList.length === 0 && styles['productCardImage'])}>
+			<div className={styles['layout']}>
+				<div
+					className={styles['imageList']}
+					ref={imageListRef}
+				>
+					{
+						imageList.map((image, index) => index < maxImages &&
+							<div
+								className={styles['image']}
+								id={String(index)}
+								ref={el => imagesRef.current[index] = el}
+								key={index}
+							>
+								<Image
+									src={image.src}
+									alt={image.alt}
+									fill
+									style={{ objectFit: 'contain' }}
+								/>
+							</div>
+						)
+					}
+				</div>
+				<div className={styles['background']} />
 			</div>
-			<div className={styles['background']} />
+
+			{
+				imageList.length > 1 &&
+				<div className={styles['snippet']}>
+					{
+						imageList.map((_, index) => index < maxImages &&
+							<div
+								className={classNames(styles['snippetItem'], index === activeImageId && styles['snippetItem--active'])}
+								key={index}
+							/>
+						)
+					}
+				</div>
+			}
 		</div>
 	)
 }
