79ebbcb21d0baee014b2adfa16689a9f2801f2c7
Andrey Grebeshov <andrey.grebeshov@gmail.com>
2023-08-28 09:44:48 +0300

    feat: productCard image swiper

--- components/ProductCardImage/index.tsx
+++ components/ProductCardImage/index.tsx
@@ -1,57 +1,32 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import classNames from 'classnames'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import Image from '../ui/Image'
 
 import { ProductCardImageProps } from './types'
 
 import styles from './styles.module.scss'
+import 'swiper/css'
 
 const maxImages = 10
 
 const ProductCardImage = ({ imageList }: ProductCardImageProps) => {
 
-	const imagesRef = useRef<Array<HTMLDivElement | null>>([])
-	const imageListRef = useRef<HTMLDivElement | null>(null)
-
 	const [activeImageId, setActiveImageId] = useState(0)
 
-	const callbackObserver: IntersectionObserverCallback = useCallback((entries) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				setActiveImageId(Number(entry.target.getAttribute('id')))
-			}
-		})
-	}, [imageList])
-
-	const initialObserve = useCallback(() => {
-
-		const observer = new IntersectionObserver(callbackObserver, {
-			root: imageListRef.current,
-			threshold: 1
-		})
-
-		imagesRef.current.forEach((imageRef) => imageRef && observer.observe(imageRef))
-	}, [imageList])
-
-	useEffect(() => {
-		imageList.length > 1 && initialObserve()
-	}, [imageList])
-
 	return (
-		<div className={classNames(imageList.length === 0 && styles['productCardImage'])}>
-			<div className={styles['layout']}>
-				<div
-					className={styles['imageList']}
-					ref={imageListRef}
-				>
-					{
-						imageList.map((image, index) => index < maxImages &&
+		<div className={styles['layout']}>
+			<Swiper
+
+			>
+				{
+					imageList.map((image, index) => index < maxImages &&
+						<SwiperSlide
+
+							key={index}
+						>
 							<div
 								className={styles['image']}
-								id={String(index)}
-								ref={el => imagesRef.current[index] = el}
-								key={index}
 							>
 								<Image
 									src={image.src}
@@ -60,25 +35,11 @@ const ProductCardImage = ({ imageList }: ProductCardImageProps) => {
 									style={{ objectFit: 'contain' }}
 								/>
 							</div>
-						)
-					}
-				</div>
-				<div className={styles['background']} />
-			</div>
-
-			{
-				imageList.length > 1 &&
-				<div className={styles['snippet']}>
-					{
-						imageList.map((_, index) => index < maxImages &&
-							<div
-								className={classNames(styles['snippetItem'], index === activeImageId && styles['snippetItem--active'])}
-								key={index}
-							/>
-						)
-					}
-				</div>
-			}
+						</SwiperSlide>
+					)
+				}
+			</Swiper>
+			<div className={styles['background']} />
 		</div>
 	)
 }
